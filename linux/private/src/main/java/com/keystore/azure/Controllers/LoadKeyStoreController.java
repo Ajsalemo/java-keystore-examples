859c01c80b3b09230b7dfaeb572db8f9ec49722b
Anthony Salemo <ajssalemo@gmail.com>
2022-10-16 17:16:51 -0400

    changing code to reference App Service keystore

--- linux/private/src/main/java/com/keystore/azure/Controllers/LoadKeyStoreController.java
+++ linux/private/src/main/java/com/keystore/azure/Controllers/LoadKeyStoreController.java
@@ -19,28 +19,15 @@ public class LoadKeyStoreController {
     public String keystore() throws KeyStoreException {
         KeyStore keyStore = KeyStore.getInstance("jks");
         try {
-            String keyStoreAlias = System.getenv("KEY_STORE_ALIAS");
-            String keyStorePassword = System.getenv("KEY_STORE_PASSWORD");
-            String firstKeyAlias = System.getenv("FIRST_KEY_ALIAS");
-            String secondKeyAlias = System.getenv("SECOND_KEY_ALIAS");
-            
-
-            char[] password = (keyStorePassword).toCharArray();
-            KeyStore.ProtectionParameter entryPassword 
-                = new KeyStore.PasswordProtection(password); 
+            char[] password = "changeit".toCharArray();
 
             keyStore.load(
-                new FileInputStream("keystore.jks"),
-                keyStoreAlias.toCharArray());
+                    new FileInputStream(System.getenv("JRE_HOME") + "/lib/security/cacerts"),
+                    password);
 
             System.out.println("Listing the KeyStore alias name..");
             System.out.println(keyStore.aliases().nextElement() + "\n");
-            System.out.println("Printing information about a KeyStore key..");
-            System.out.println(keyStore.getKey(firstKeyAlias, password) + "\n");
-            System.out.println("Printing information about a KeyStore entry..");
-            System.out.println(keyStore.getEntry(secondKeyAlias, entryPassword) + "\n");
-
-        } catch (NoSuchAlgorithmException | CertificateException | IOException | UnrecoverableEntryException e) {
+        } catch (NoSuchAlgorithmException | CertificateException | IOException e) {
             e.printStackTrace();
         }
 
