ef673e404897f3c95a1c2817439af3c76ff3a55c
Anthony Salemo <ajssalemo@gmail.com>
2022-10-23 18:56:10 -0400

    adding scaffolded projects for windows keystores

---
+++ windows/private/src/test/java/com/keystore/azure/AzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keystore.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
