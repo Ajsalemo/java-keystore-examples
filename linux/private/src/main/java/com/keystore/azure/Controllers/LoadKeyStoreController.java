ee1cb05c73a43288016c8a772fbc5c1c165fac07
Anthony Salemo <ajssalemo@gmail.com>
2022-10-27 22:53:06 -0400

    able to successfully access a private keystore on linux

--- linux/private/src/main/java/com/keystore/azure/Controllers/LoadKeyStoreController.java
+++ linux/private/src/main/java/com/keystore/azure/Controllers/LoadKeyStoreController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,17 +17,23 @@ public class LoadKeyStoreController {
 
     @GetMapping("/api/keystore")
     public String keystore() throws KeyStoreException {
-        KeyStore keyStore = KeyStore.getInstance("jks");
+        KeyStore keyStore = KeyStore.getInstance("pkcs12");
         try {
             char[] password = "changeit".toCharArray();
+            KeyStore.ProtectionParameter entryPassword
+                = new KeyStore.PasswordProtection(password);
 
             keyStore.load(
-                    new FileInputStream(System.getenv("JRE_HOME") + "/lib/security/cacerts"),
+                    new FileInputStream(System.getenv("JRE_HOME") + "/lib/security/client.jks"),
                     password);
 
             System.out.println("Listing the KeyStore alias name..");
             System.out.println(keyStore.aliases().nextElement() + "\n");
-        } catch (NoSuchAlgorithmException | CertificateException | IOException e) {
+            // The Alias required for the getEntry argument is the Certificate thumbprint
+            // CERTIFICATE_THUMBPRINT is an arbitrary App Setting to be manually set to the thumbprint
+            System.out.println(keyStore.getEntry(System.getenv("CERTIFICATE_THUMBPRINT"), entryPassword));
+            System.out.println(keyStore.containsAlias(System.getenv("CERTIFICATE_THUMBPRINT")));
+        } catch (NoSuchAlgorithmException | CertificateException | IOException | UnrecoverableEntryException e) {
             e.printStackTrace();
         }
 
